f01d54339d288d10d2dbda8f9e4cb6c9d3c2a47c
saumya <saumya20personal@gmail.com>
2026-05-05 10:20:18 +0530

    mongo db error

--- server/index.js
+++ server/index.js
@@ -31,8 +31,17 @@ app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .then(() => {
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 // Routes
 app.use('/api/auth', authRoutes);
